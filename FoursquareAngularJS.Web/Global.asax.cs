04e0aeaf2388ab508a16d892b2f2abe0419b1fec
Taiseer Joudeh <tayseer_joudeh@hotmail.com>
2014-05-03 01:05:10 +0300

    Remove MVC routing

--- FoursquareAngularJS.Web/Global.asax.cs
+++ FoursquareAngularJS.Web/Global.asax.cs
@@ -20,7 +20,7 @@ namespace FoursquareAngularJS.Web
 
             WebApiConfig.Register(GlobalConfiguration.Configuration);
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
-            RouteConfig.RegisterRoutes(RouteTable.Routes);
+            //RouteConfig.RegisterRoutes(RouteTable.Routes);
             //BundleConfig.RegisterBundles(BundleTable.Bundles);
         }
     }
